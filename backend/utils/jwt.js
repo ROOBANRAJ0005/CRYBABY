3ef1a74884278cabc907dde8f2ada9757d7c2c02
ROOBANRAJ0005 <roobanraj0005@gmail.com>
2025-12-20 17:38:15 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,16 +1,23 @@
 const sendToken = (user,statusCode,res)=>{
     const token = user.getUserToken();
 
-    const options = {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME *24 * 60 * 60 * 1000),
-        httpOnly:true
-    }
+  const options = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // 🔥 REQUIRED
+  sameSite: "none",                               // 🔥 REQUIRED
+  expires: new Date(
+    Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+  )
+};
 
-    res.status(statusCode).cookie('token', token, options).json({
-        success:true,
-        user,
-        token
-    })
+    res
+  .status(statusCode)
+  .cookie("token", token, options)
+  .json({
+    success: true,
+    user,
+    token
+  });
 }
 
 module.exports = sendToken;
