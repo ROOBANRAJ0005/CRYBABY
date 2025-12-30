26bc87bc404d00bfd0f51fdbd9538aeb4cffa16b
ROOBANRAJ0005 <roobanraj0005@gmail.com>
2025-12-30 12:17:52 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,13 @@
   import store from './store';
   import './index.css'
   import { BrowserRouter } from 'react-router-dom';
-  import { SpeedInsights } from '@vercel/speed-insights/react';
+ 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
       <BrowserRouter>
       <Provider store={store}>
           <App />
-          <SpeedInsights/>
       </Provider>
     
       </BrowserRouter>
